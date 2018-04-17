41c07cabb24e6135f2ecbefae3673ecd1b974e11
fangyu <446529127@qq.com>
2018-04-17 22:47:57 +0800

    config

---
+++ www/config_default.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+__author__ = 'fangyu'
+
+configs = {
+	'db':{
+		'host':'127.0.0.1',
+		'port':3306,
+		'user':'www-data',
+		'database':'awesome'
+	},
+	'session':{
+		'secret':'AwEsOmE'
+	}
+}
---
+++ www/config_override.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__author__ = 'fangyu'
+
+config = {
+	'db':{
+		'host':'127.0.0.1'
+	}
+}
