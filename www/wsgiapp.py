46a12e6442a92feeae982a84b57c6c08a9bfa3a3
ForYou <446529127@qq.com>
2018-09-13 16:04:51 +0800

    add comments

--- www/wsgiapp.py
+++ www/wsgiapp.py
@@ -19,7 +19,7 @@ def datetime_filter(t):
 	if delta < 60:
 		return u'1分钟前'
 	if delta < 3600:
-		return u'%s分钟前' % (delta // 60)
+		return u'%s分钟前' % (delta // 60)  # //地板除法 舍去小数
 	if delta < 86400:
 		return u'%s小时前' % (delta // 3600)
 	if delta < 604800:
